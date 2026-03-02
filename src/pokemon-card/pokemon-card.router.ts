2990635e11b6a2417571388eb3f2070391c7d8de
RegnaultAlex <regnaultalexpro@gmail.com>
2026-03-02 15:41:48 +0100

    ajout login/token, securisation routes avec token

--- src/pokemon-card/pokemon-card.router.ts
+++ src/pokemon-card/pokemon-card.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createPokemonCard, deletePokemonCard, editPokemonCard, getPokemonCard, getPokemonCards } from "./pokemon-card.controller";
+import { verifyJWT } from "./pokemon-card.middleware";
 
 export const pokemonCardRouter = Router()
 
 
 pokemonCardRouter.get("/", getPokemonCards);
 pokemonCardRouter.get("/:pokemonCardId", getPokemonCard);
-pokemonCardRouter.post("/", createPokemonCard)
-pokemonCardRouter.patch("/:pokemonCardId", editPokemonCard)
-pokemonCardRouter.delete("/:pokemonCardId", deletePokemonCard);
+pokemonCardRouter.post("/", verifyJWT, createPokemonCard)
+pokemonCardRouter.patch("/:pokemonCardId", verifyJWT, editPokemonCard)
+pokemonCardRouter.delete("/:pokemonCardId", verifyJWT, deletePokemonCard);
